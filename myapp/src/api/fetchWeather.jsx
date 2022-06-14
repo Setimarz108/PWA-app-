b484f7b2e158fb2b2f4f2269fb26171a74e6b2d8
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2022-06-14 14:27:46 +0200

    first commit

---
+++ myapp/src/api/fetchWeather.jsx
@@ -0,0 +1,20 @@
+
+import axios from 'axios'
+
+const URL = 'http://api.openweathermap.org/data/2.5/weather'
+const API_KEY ='117a53594cb9a257a4814c897429b283'
+ 
+ export  const fetchWeather = async (query) => {
+
+    const {data} = await axios.get(URL, {
+        params: {
+            q: query,
+            units: 'metric',
+            APPID: API_KEY
+        }
+    });
+
+    return data;
+
+}
+
